39f8f881bed7390460d3b86ed0757e7cc5bcbfcd
wangchen <wangvsa@gmail.com>
2013-07-04 13:56:25 +0000

    use highcharts to draw the charts since we can't use google charts offline

--- README.md
+++ README.md
@@ -2,5 +2,3 @@
 
 Web Timer is a Chrome extension designed to help you keep track of how
 you're using your time online.
-
-Check it out in the [Chrome Web Store](http://webtimerapp.com)!
--- popup.js
+++ popup.js
@@ -1,43 +1,87 @@
 var bg = chrome.extension.getBackgroundPage();
 
-// Load the Visualization API and the piechart package.
-google.load('visualization', '1.0', {'packages':['corechart', 'table']});
-// Set a callback to run when the Google Visualization API is loaded.
-if (top === self) {
-  google.setOnLoadCallback(function() { show(bg.TYPE.today); });
-} else {
-  // For screenshot: if in iframe, load the most recently viewed mode
-  google.setOnLoadCallback(function () {
-    if (bg.mode === bg.TYPE.today) {
-      show(bg.TYPE.today);
-    } else if (bg.mode === bg.TYPE.average) {
-      show(bg.TYPE.average);
-    } else if (bg.mode === bg.TYPE.all) {
-      show(bg.TYPE.all);
-    } else {
-      console.error("No such type: " + bg.mode);
-    }
-  });
-}
+$(function () { 
+    var chartData = getPieData(bg.mode);
+    $('#chart_div').highcharts({
+	title: { text: null },
+	chart :{
+	    margin: [0,0,0,0],
+	    height: 250
+	},
+	tooltip: {
+	    valueDecimals: 1,
+	    pointFormat: '<b>{point.percentage}%</b>'
+	},
+	plotOptions: {
+	    pie: {
+		allowPointSelect: true,
+		cursor: 'pointer',
+		dataLabels: {
+		    formatter: function() {
+			return '<b>'+this.point.name+'</b>';
+		    }
+		}
+	    }
+	},
+	series: [{
+	    type: 'pie',
+	    data: chartData
+	}]
+    });
 
-// Show options in a new tab
-function showOptions() {
-  chrome.tabs.create({
-    url: 'options.html'
-  });
-}
+    var domains = [];
+    var usages = [];
+    for(var i=0;i<chartData.length;i++) {
+	domains.push(chartData[i][0]);
+	usages.push(chartData[i][1]);
+    }
+
+    $('#table_div').highcharts({
+	chart: { 
+	    type: 'column',
+	    height: 280
+	},
+	title: { text: null },
+	xAxis: { 
+	    categories: domains,
+	    labels: {
+		rotation: -45,
+		align: 'right'
+	    }
+	},
+	yAxis: { title: { text: 'time usage' } },
+	tooltip: {
+	    formatter: function() {
+		return this.x+": <b>"+timeString(this.y)+'</b>';
+	    }
+	},
+	legend : {
+	    enabled: false
+	},
+	series: [{ 
+	    data:usages,
+	    dataLabels: {
+		enabled: true,
+		formatter: function() {
+		    return '<b>'+timeString(this.y)+'</b>';
+		}
+	    }
+	}]
+    });
+});
+
 
 // Converts duration to String
 function timeString(numSeconds) {
   if (numSeconds === 0) {
-    return "0 seconds";
+    return "0s";
   }
   var remainder = numSeconds;
   var timeStr = "";
   var timeTerms = {
-    hour: 3600,
-    minute: 60,
-    second: 1
+    h: 3600,
+    m: 60,
+    s: 1
   };
   // Don't show seconds if time is more than one hour
   if (remainder >= timeTerms.hour) {
@@ -49,22 +93,23 @@ function timeString(numSeconds) {
     var divisor = timeTerms[term];
     if (remainder >= divisor) {
       var numUnits = Math.floor(remainder / divisor);
-      timeStr += numUnits + " " + term;
+      timeStr += numUnits + "" + term;
       // Make it plural
-      if (numUnits > 1) {
-        timeStr += "s";
-      }
+      //if (numUnits > 1) {
+      //  timeStr += "s";
+      //}
       remainder = remainder % divisor;
       if (remainder) {
-        timeStr += " and ";
+        timeStr += "";
       }
     }
   }
   return timeStr;
 }
 
+
 // Show the data for the time period indicated by addon
-function displayData(type) {
+function getPieData(type) {
   // Get the domain data
   var domains = JSON.parse(localStorage["domains"]);
   var chart_data = [];
@@ -81,13 +126,7 @@ function displayData(type) {
       console.error("No such type: " + type);
     }
     if (numSeconds > 0) {
-      chart_data.push([domain, {
-        v: numSeconds,
-        f: timeString(numSeconds),
-        p: {
-          style: "text-align: left; white-space: normal;"
-        }
-      }]);
+      chart_data.push([domain, numSeconds]);
     }
   }
 
@@ -100,24 +139,24 @@ function displayData(type) {
 
   // Sort data by descending duration
   chart_data.sort(function (a, b) {
-    return b[1].v - a[1].v;
+    return b[1] - a[1];
   });
 
   // Limit chart data
   var limited_data = [];
   var chart_limit;
-  // For screenshot: if in iframe, image should always have 9 items
+  // For screenshot: if in iframe, image should always have 8 items
   if (top == self) {
     chart_limit = parseInt(localStorage["chart_limit"], 10);
   } else {
-    chart_limit = 9;
+    chart_limit = 8;
   }
   for (var i = 0; i < chart_limit && i < chart_data.length; i++) {
     limited_data.push(chart_data[i]);
   }
   var sum = 0;
   for (var i = chart_limit; i < chart_data.length; i++) {
-    sum += chart_data[i][1].v;
+    sum += chart_data[i][1];
   }
   // Add time in "other" category for total and average
   var other = JSON.parse(localStorage["other"]);
@@ -127,45 +166,10 @@ function displayData(type) {
     sum += other.all;
   }
   if (sum > 0) {
-    limited_data.push(["Other", {
-      v: sum,
-      f: timeString(sum),
-      p: {
-        style: "text-align: left; white-space: normal;"
-      }
-    }]);
+    limited_data.push(["Other", sum]);
   }
 
-  // Draw the chart
-  drawChart(limited_data);
-
-  // Add total time
-  var total = JSON.parse(localStorage["total"]);
-  var numSeconds = 0;
-  if (type === bg.TYPE.today) {
-    numSeconds = total.today;
-  } else if (type === bg.TYPE.average) {
-    numSeconds = Math.floor(total.all / parseInt(localStorage["num_days"], 10));
-  } else if (type === bg.TYPE.all) {
-    numSeconds = total.all;
-  } else {
-    console.error("No such type: " + type);
-  }
-  limited_data.push([{
-    v: "Total",
-    p: {
-      style: "font-weight: bold;"
-    }
-  }, {
-    v: numSeconds,
-    f: timeString(numSeconds),
-    p: {
-      style: "text-align: left; white-space: normal; font-weight: bold;"
-    }
-  }]);
-
-  // Draw the table
-  drawTable(limited_data, type);
+  return limited_data;
 }
 
 function updateNav(type) {
@@ -177,60 +181,21 @@ function updateNav(type) {
 
 function show(mode) {
   bg.mode = mode;
-  displayData(mode);
+  var chart = $('#chart_div').highcharts();
+  var chart = $('#table_div').highcharts();
+  chart.redraw();
   updateNav(mode);
 }
 
-// Callback that creates and populates a data table,
-// instantiates the pie chart, passes in the data and
-// draws it.
-function drawChart(chart_data) {
-  // Create the data table.
-  var data = new google.visualization.DataTable();
-  data.addColumn('string', 'Domain');
-  data.addColumn('number', 'Time');
-  data.addRows(chart_data);
 
-  // Set chart options
-  var options = {
-    tooltip: {
-      text: 'percentage'
-    },
-    chartArea: {
-      width: 400,
-      height: 180
-    }
-  };
-
-  // Instantiate and draw our chart, passing in some options.
-  var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-  chart.draw(data, options);
+// Show options in a new tab
+function showOptions() {
+  chrome.tabs.create({
+    url: 'options.html'
+  });
 }
 
-function drawTable(table_data, type) {
-  var data = new google.visualization.DataTable();
-  data.addColumn('string', 'Domain');
-  var timeDesc;
-  if (type === bg.TYPE.today) {
-    timeDesc = "Today";
-  } else if (type === bg.TYPE.average) {
-    timeDesc = "Daily Average";
-  } else if (type === bg.TYPE.all) {
-    timeDesc = "Over " + localStorage["num_days"] + " Days";
-  } else {
-    console.error("No such type: " + type);
-  }
-  data.addColumn('number', "Time Spent (" + timeDesc + ")");
-  data.addRows(table_data);
-
-  var options = {
-    allowHtml: true,
-    sort: 'disable'
-  };
-  var table = new google.visualization.Table(document.getElementById('table_div'));
-  table.draw(data, options);
-}
-
+// Open share's tab
 function share() {
   chrome.tabs.create({
     url: 'share.html'
@@ -238,10 +203,10 @@ function share() {
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-  document.querySelector('#today').addEventListener('click', function() { show(bg.TYPE.today); });
-  document.querySelector('#average').addEventListener('click', function() { show(bg.TYPE.average); });
-  document.querySelector('#all').addEventListener('click', function() { show(bg.TYPE.all); });
+    document.querySelector('#today').addEventListener('click', function() { show(bg.TYPE.today); });
+    document.querySelector('#average').addEventListener('click', function() { show(bg.TYPE.average); });
+    document.querySelector('#all').addEventListener('click', function() { show(bg.TYPE.all); });
 
-  document.querySelector('#options').addEventListener('click', showOptions);
-  document.querySelector('#share').addEventListener('click', share);
+    document.querySelector('#options').addEventListener('click', showOptions);
+    document.querySelector('#share').addEventListener('click', share);
 });
